57f986d65240113a1395b7709e86338743922bdb
publizm <publizm@gmail.com>
2020-01-31 20:32:27 +0900

    study type and interface setting

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var person_1 = __importDefault(require("./person"));
 var person = new person_1.default('Lee');
 // 타입추론
-var foo = 123;
-foo = 'hello';
+// let foo = 123;
+// foo = 'hello';
 person.sayHi();
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import Person from './person';
 const person = new Person('Lee');
 
 // 타입추론
-let foo = 123;
-foo = 'hello';
+// let foo = 123;
+// foo = 'hello';
 
 
 person.sayHi();
