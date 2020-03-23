fe88da5e1e9e65eccf7d9470e770726239d6eb8b
publizm <publizm@gmail.com>
2020-03-23 17:36:33 +0900

    docs: 타입스크립트로 form 구현해보기

--- ts-react/src/App.tsx
+++ ts-react/src/App.tsx
@@ -3,16 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 import Greetings from './Greetings';
 import Counter from './Counter';
+import MyForm from './MyForm';
 
 function App() {
   const checkName = (name: string) => {
     alert(name);
   }
 
+  const onSubmit = (form: {name: string, description: string}) => {
+    console.log(form);
+  }
+
   return (
     <>
       <Greetings onClick={checkName} />
       <Counter />
+      <MyForm onSubmit={onSubmit} />
     </>
   );
 }
